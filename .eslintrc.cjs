84cfe60666d7120a7a854007715ca6de07d1653d
The Vu <thevu091193@gmail.com>
2025-09-29 21:33:53 +0700

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
+/* eslint-env node */
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react-hooks/recommended",
+    // Thêm 2 dòng dưới nếu bạn muốn rule cho React như README gợi ý:
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
   },
+  plugins: ["react-refresh"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"], // giữ nguyên nếu bạn muốn bỏ qua chính file cấu hình
+  rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    // có thể thêm rule tùy team ở đây
+  },
+  settings: {
+    // giúp eslint-plugin-react tự detect version
+    react: { version: "detect" }
+  }
 };
